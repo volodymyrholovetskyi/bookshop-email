a871aa85b5c610b51cf32c36bdaacbb9e09f6bd8
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-05-30 20:20:51 +0200

    Start commit

---
+++ src/main/java/ua/vholovetskyi/orderemail/OrderEmailApplication.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.orderemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/vholovetskyi/orderemail/OrderEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.orderemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
